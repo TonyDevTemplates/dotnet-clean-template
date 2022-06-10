79f46b582ac9f7e7bfb3cb7c4429e9e25c75e099
Thonifho Muhali <73519549+tony-rsa@users.noreply.github.com>
2022-06-10 18:26:11 +0200

    Initialize dependabot/npm_and_yarn/src/CleanTemplate.WebUI/ClientApp/node-sass-7.0.0

---
+++ src/CleanTemplate.Application/Common/ValidationPipeline.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+
+namespace CleanTemplate.Application.Common
+{
+    public class ValidationPipeline<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationPipeline(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (!_validators.Any()) 
+                return await next();
+
+            var context = new ValidationContext(request);
+
+            var result = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+            var failed = result.SelectMany(task => task.Errors).Where(message => message != null).ToList();
+
+            if (failed.Count != 0)
+            {
+                throw new ServerSideValidationException(failed);
+            }
+
+            return await next();
+        }
+    }
+}

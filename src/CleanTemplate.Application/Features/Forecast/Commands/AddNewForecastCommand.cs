737a3ab3f77280163398f8bf9f729ca702d188be
Thonifho Muhali <73519549+tony-rsa@users.noreply.github.com>
2022-06-10 18:26:12 +0200

    Initialize dependabot/npm_and_yarn/src/CleanTemplate.WebUI/ClientApp/url-parse-1.5.10

---
+++ src/CleanTemplate.Application/Features/Forecast/Commands/AddNewForecastCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CleanTemplate.Application.Common;
+using CleanTemplate.Domain.Entities.Forecasts;
+using CleanTemplate.Domain.Entities.Locations;
+using MediatR;
+
+namespace CleanTemplate.Application.Features.Forecast.Commands
+{
+    public class AddNewForecastCommand : BaseCqrsRequest<Guid>
+    {
+        public AddNewForecastCommand(string appUser, WeatherForecastCreation creationData)
+            : base(appUser)
+        {
+            CreationData = creationData;
+        }
+
+        public WeatherForecastCreation CreationData { get; }
+
+    }
+
+    internal class AddNewForecastCommandHandler
+        : IRequestHandler<AddNewForecastCommand, Guid>
+    {
+        private readonly IForecastDbContext _forecastDbContext;
+
+        public AddNewForecastCommandHandler(IForecastDbContext forecastDbContext)
+        {
+            _forecastDbContext = forecastDbContext;
+        }
+
+        public async Task<Guid> Handle(AddNewForecastCommand request, CancellationToken cancellationToken)
+        {
+            var forecast = new WeatherForecast
+            {
+                Clouds = request.CreationData.Clouds,
+                ForecastDate = request.CreationData.ForecastDate,
+                ForecastLocation = new Location { Id = request.CreationData.Location },
+                Summary = request.CreationData.Summary,
+                Temperature = request.CreationData.Temperature,
+                Wind = request.CreationData.Wind,
+            };
+
+            _forecastDbContext.WeatherForecasts.Add(forecast);
+
+            await _forecastDbContext.SaveChangesAsync(cancellationToken);
+
+            return forecast.Id;
+        }
+    }
+}

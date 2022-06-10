99894c121dc00d34210244085b1c1bba53bc29ec
Thonifho Muhali <73519549+tony-rsa@users.noreply.github.com>
2022-06-10 18:26:11 +0200

    Initialize dependabot/npm_and_yarn/src/CleanTemplate.WebUI/ClientApp/path-parse-1.0.7

---
+++ src/CleanTemplate.Domain/Entities/Forecasts/WeatherForecast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CleanTemplate.Domain.Common;
+using CleanTemplate.Domain.Entities.Locations;
+
+namespace CleanTemplate.Domain.Entities.Forecasts
+{
+    public class WeatherForecast : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+        public int Temperature { get; set; }
+        public string Wind { get; set; }
+        public string Clouds { get; set; }
+        public DateTime ForecastDate { get; set; }
+        public string Summary { get; set; }
+
+        public Location ForecastLocation { get; set; }
+    }
+}

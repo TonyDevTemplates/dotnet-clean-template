449948f3ce886a7a07dfcec04dadf483c0b474c7
Thonifho Muhali <73519549+tony-rsa@users.noreply.github.com>
2022-06-10 18:26:12 +0200

    Initialize dependabot/npm_and_yarn/src/CleanTemplate.WebUI/ClientApp/follow-redirects-1.14.8

---
+++ src/CleanTemplate.Persistence.Database/Configurations/LocationConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using CleanTemplate.Domain.Entities.Locations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanTemplate.Persistence.Database.Configurations
+{
+    public class LocationConfiguration : IEntityTypeConfiguration<Location>
+    {
+        public void Configure(EntityTypeBuilder<Location> builder)
+        {
+            builder.Property(t => t.City)
+                .HasMaxLength(200)
+                .IsRequired();
+        }
+    }
+}

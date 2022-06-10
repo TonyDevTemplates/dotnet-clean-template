5b448bb37b041985ab1c3f3eca82926352bca4f1
Thonifho Muhali <73519549+tony-rsa@users.noreply.github.com>
2022-06-10 18:26:06 +0200

    Initialize dependabot/npm_and_yarn/src/CleanTemplate.WebUI/ClientApp/elliptic-6.5.4

---
+++ src/CleanTemplate.Application/Features/Forecast/Commands/AddNewForecastValidation.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace CleanTemplate.Application.Features.Forecast.Commands
+{
+    public class AddNewForecastValidation : AbstractValidator<AddNewForecastCommand>
+    {
+        public AddNewForecastValidation()
+        {
+            RuleFor(x => x.CreationData.Summary).NotEmpty();
+        }
+    }
+}
---
+++ test/CleanTemplate.UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CleanTemplate.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

d5d7db10e547b228cc1611cddec8b48ae00707b2
Thonifho Muhali <73519549+tony-rsa@users.noreply.github.com>
2022-06-10 18:26:11 +0200

    Initialize dependabot/npm_and_yarn/src/CleanTemplate.WebUI/ClientApp/log4js-6.4.0

---
+++ src/CleanTemplate.Domain/Entities/Locations/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using CleanTemplate.Domain.Common;
+using CleanTemplate.Domain.Entities.Forecasts;
+
+namespace CleanTemplate.Domain.Entities.Locations
+{
+    public class Location : IEntity<Guid>
+    {
+        public Location()
+        {
+            Forecasts = new List<WeatherForecast>();
+        }
+
+        public Guid Id { get; set; }
+        
+        public string City { get; set; }
+
+        public IList<WeatherForecast> Forecasts { get; }
+    }
+}
---
+++ src/CleanTemplate.Persistence.Database/ForecastDbContext.cs
@@ -0,0 +1,87 @@
+﻿using System.Data;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using CleanTemplate.Application.Common;
+using CleanTemplate.Domain.Entities.Forecasts;
+using CleanTemplate.Domain.Entities.Locations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace CleanTemplate.Persistence.Database
+{
+    public class ForecastDbContext : DbContext, IForecastDbContext
+    {
+        private IDbContextTransaction _currentTransaction;
+
+        public ForecastDbContext(DbContextOptions<ForecastDbContext> options) :
+            base(options)
+        {
+        }
+
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+
+        public DbSet<Location> Locations { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            if (_currentTransaction != null)
+            {
+                return;
+            }
+
+            _currentTransaction = await base.Database.BeginTransactionAsync(IsolationLevel.ReadCommitted).ConfigureAwait(false);
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            try
+            {
+                await SaveChangesAsync().ConfigureAwait(false);
+
+                _currentTransaction?.Commit();
+            }
+            catch
+            {
+                RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public void RollbackTransaction()
+        {
+            try
+            {
+                _currentTransaction?.Rollback();
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

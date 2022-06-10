4e957495d2f3c0878562bffa7aff10348dab3208
Thonifho Muhali <73519549+tony-rsa@users.noreply.github.com>
2022-06-10 18:26:08 +0200

    Initialize dependabot/npm_and_yarn/src/CleanTemplate.WebUI/ClientApp/ssri-6.0.2

---
+++ test/CleanTemplate.UnitTest/App/Features/Forecast/ForecastTestBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using CleanTemplate.Application.Common;
+using CleanTemplate.Domain.Entities.Forecasts;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+
+namespace CleanTemplate.UnitTest.App.Features.Forecast
+{
+    [TestFixture]
+    internal class ForecastTestBase
+    {
+        protected Mock<IForecastDbContext> ForecastDbStub;
+
+
+        private Mock<IForecastDbContext> CreatePbxStorageMock()
+        {
+            var forecast = new WeatherForecast
+            {
+                Temperature = 10,
+                Wind = "WEST",
+                Clouds = "LOW",
+                Summary = "Summary",
+                ForecastDate = new DateTime(2020, 10, 10),
+                Id = Guid.NewGuid()
+            };
+            var forecasts = new List<WeatherForecast> { forecast };
+
+            var mockSet = new Mock<DbSet<WeatherForecast>>();
+
+            var storageMock = new Mock<IForecastDbContext>();
+            storageMock.Setup(x => x.WeatherForecasts).Returns(mockSet.Object);
+
+
+            return storageMock;
+        }
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            ForecastDbStub = CreatePbxStorageMock();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ForecastDbStub = null;
+        }
+    }
+}
---
+++ test/CleanTemplate.UnitTest/Infrastructure/Services/SystemDateTimeServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CleanTemplate.Application.Common;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace CleanTemplate.UnitTest.Infrastructure.Services
+{
+    [TestFixture]
+    internal class SystemDateTimeServiceTests
+    {
+        private Mock<IDateService> _dateService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _dateService = CreateDateServiceStub();
+        }
+
+        private static Mock<IDateService> CreateDateServiceStub()
+        {
+            var dateStub = new Mock<IDateService>();
+
+            dateStub.Setup(x => x.Now).Returns(new DateTime(1010, 10, 10));
+
+            return dateStub;
+        }
+
+        [TearDown]
+        public void Destroy()
+        {
+            _dateService = null;
+        }
+
+        [Test]
+        public void GetNow_GetPredictedNowDate_ReturnDate()
+        {
+            var expected = new DateTime(1010, 10, 10);
+            var testStub = _dateService.Object;
+
+            var result = testStub.Now;
+
+            result.Should().BeCloseTo(expected);
+        }
+    }
+}

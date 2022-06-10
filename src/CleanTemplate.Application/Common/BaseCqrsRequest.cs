0098af88ecad805f2f5691301224d98920535243
Thonifho Muhali <73519549+tony-rsa@users.noreply.github.com>
2022-06-10 18:26:10 +0200

    Initialize dependabot/npm_and_yarn/src/CleanTemplate.WebUI/ClientApp/dns-packet-1.3.4

---
+++ src/CleanTemplate.Application/Common/BaseCqrsRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace CleanTemplate.Application.Common
+{
+    public class BaseCqrsRequest<T> : IRequest<T>
+    {
+        protected BaseCqrsRequest(string appUser)
+        {
+            AppUser = appUser;
+        }
+
+        public string AppUser { get; }
+    }
+}
---
+++ src/CleanTemplate.Domain/Entities/Forecasts/WeatherForecastCreation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CleanTemplate.Domain.Entities.Forecasts
+{
+    public class WeatherForecastCreation
+    {
+        public int Temperature { get; set; }
+        public string Wind { get; set; }
+        public string Clouds { get; set; }
+        public DateTime ForecastDate { get; set; }
+        public string Summary { get; set; }
+
+        public Guid Location { get; set; }
+    }
+}

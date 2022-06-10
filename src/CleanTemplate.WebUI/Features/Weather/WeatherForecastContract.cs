591ac6bf93e277ea5b03f68ff8ba9c155783ddb1
Thonifho Muhali <73519549+tony-rsa@users.noreply.github.com>
2022-06-10 18:26:05 +0200

    Initial commit

---
+++ src/CleanTemplate.WebUI/Features/Weather/WeatherForecastContract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CleanTemplate.WebUI.Features.Weather
+{
+    public class WeatherForecastContract
+    {
+        public Guid Id { get; set; }
+        public int Temperature { get; set; }
+        public string Wind { get; set; }
+        public string Clouds { get; set; }
+        public DateTime ForecastDate { get; set; }
+        public string Summary { get; set; }
+
+        public string LocationName { get; set; }
+    }
+}

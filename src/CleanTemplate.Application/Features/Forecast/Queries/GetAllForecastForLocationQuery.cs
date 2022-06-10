7ad6e6ce9d6f7cc73e0712be7cb4f21f63970f8f
Thonifho Muhali <73519549+tony-rsa@users.noreply.github.com>
2022-06-10 18:26:05 +0200

    Initialize dependabot/npm_and_yarn/src/CleanTemplate.WebUI/ClientApp/socket.io-2.4.1

---
+++ src/CleanTemplate.Application/Features/Forecast/Queries/GetAllForecastForLocationQuery.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using CleanTemplate.Application.Common;
+using CleanTemplate.Domain.Entities.Forecasts;
+
+using MediatR;
+
+namespace CleanTemplate.Application.Features.Forecast.Queries
+{
+    public class GetAllForecastForLocationQuery : BaseCqrsRequest<IQueryable<WeatherForecast>>
+    {
+        public GetAllForecastForLocationQuery(string appUser, string location)
+            : base(appUser)
+        {
+            Location = location;
+        }
+
+        public string Location { get; }
+    }
+
+    internal class GetAllForecastForLocationQueryHandler
+        : IRequestHandler<GetAllForecastForLocationQuery, IQueryable<WeatherForecast>>
+    {
+
+        public GetAllForecastForLocationQueryHandler()
+        {
+        }
+
+        public Task<IQueryable<WeatherForecast>> Handle(GetAllForecastForLocationQuery request, CancellationToken cancellationToken)
+        {
+
+            return Task.FromResult<IQueryable<WeatherForecast>>(null);
+        }
+    }
+}
